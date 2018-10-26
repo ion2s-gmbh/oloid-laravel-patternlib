78033b74946e7194ec5cb8d56882ff8e3e63a531
Sebastian Baum <sebastian.baum@ion2s.com>
2018-10-26 13:42:08 +0200

    added webpack.mix.js file

---
+++ webpack.mix.js
@@ -0,0 +1,34 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js');
+
+mix.sass('resources/sass/app.scss', 'public/css');
+
+
+/*
+|-----------------------------------------------------------------------
+| BrowserSync
+|-----------------------------------------------------------------------
+|
+| BrowserSync refreshes the Browser if file changes (js, sass, php) are | detected.
+| Proxy specifies the location where the app is located.
+*/
+mix.browserSync({
+  proxy: 'http://localhost',
+  host: 'localhost',
+  open: false,
+  watchOptions: {
+    usePolling: true
+  }
+});

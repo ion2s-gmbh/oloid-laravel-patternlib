a7aa23021e8243dd7a5899dbd3c8f7b53d24268c
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-01 15:12:25 +0100

    added jquery from cdn temporarily

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,10 @@
 
     <!-- Scripts -->
     <script src="{{ asset('vendor/ion2s/laratomics-workshop/js/app.js') }}" defer></script>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" defer></script>
 
     <!-- Fonts -->

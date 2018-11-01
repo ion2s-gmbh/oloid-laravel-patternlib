e4e28c6013a6db1b19c74d4df28f01b69ad9fd90
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-01 14:32:13 +0100

    added routes from prototype

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,22 @@
 <?php
 
-Route::get('/', function () {
+use Illuminate\Support\Facades\Route;
+
+Route::get(config('/'), function () {
     return view('laratomics-workshop::home');
-});
+})->name('workshop');
+
+Route::post('laratomics/publish/{pattern}', 'PublishController@publish')
+    ->name('publish');
+
+Route::get('laratomics/create-pattern', 'PatternController@createForm')
+    ->name('create-pattern');
+
+Route::post('laratomics/store-pattern', 'PatternController@store')
+    ->name('store-pattern');
+
+Route::get('/laratomics/{pattern}', 'PreviewController@previewLocal')
+    ->name('preview-pattern');
+
+Route::get('/laratomics/preview/{pattern}', 'PreviewController@getPreview')
+    ->name('get-preview');

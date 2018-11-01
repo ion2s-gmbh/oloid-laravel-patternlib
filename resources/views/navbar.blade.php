4981c9753e21ac6890ef994583794ef8071839e1
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-01 15:15:06 +0100

    loading navbar using a filesystem configuration added config documentation in README.md

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,4 +1,4 @@
-{{--@inject('navbar', 'Ion2s\Laratomics\Services\NavbarService)--}}
+@inject('navbar', 'Ion2s\Laratomics\Services\NavbarService)
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
@@ -8,23 +8,23 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
-            {{--@foreach($navbar->sections() as $section)--}}
+            @foreach($navbar->sections() as $section)
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{--{{ strtoupper($section) }}--}}
+                        {{ strtoupper($section) }}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-{{--                        @foreach($navbar->patterns($section) as $pattern)--}}
-                            {{--<a href="/laratomics/{{ $navbar->slug($pattern) }}">{{ $pattern }}</a><br>--}}
-                        {{--@endforeach--}}
+                        @foreach($navbar->patterns($section) as $pattern)
+                            <a href="/laratomics/{{ $navbar->slug($pattern) }}">{{ $pattern }}</a><br>
+                        @endforeach
                     </div>
                 </li>
-            {{--@endforeach--}}
+            @endforeach
         </ul>
-        {{--<a href="{{ route('create-pattern') }}">--}}
+        <a href="{{ route('create-pattern') }}">
             <button class="btn btn-primary">
                 <i class="fas fa-plus-square"></i>
             </button>
-        {{--</a>--}}
+        </a>
     </div>
 </nav>

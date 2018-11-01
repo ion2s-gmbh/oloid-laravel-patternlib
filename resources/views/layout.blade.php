f10510aa4703244c5a1b50e2b4626165ee6c72af
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-01 14:32:50 +0100

    moved layout and home view from prototype

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,18 +11,24 @@
 
     <!-- Scripts -->
     <script src="{{ asset('vendor/ion2s/laratomics-workshop/js/app.js') }}" defer></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"
+          integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+
 
     <!-- Styles -->
-    {{--<link href="{{ asset('vendor/ion2s/laratomics-workshop/css/app.css') }}" rel="stylesheet">--}}
-    {{--<link href='{{mix('app.css', 'vendor/laratomics-workshop')}}' rel='stylesheet' type='text/css'>--}}
     <link href='{{ asset('vendor/ion2s/laratomics-workshop/css/app.css') }}' rel='stylesheet' type='text/css'>
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' rel='stylesheet'
+          type='text/css'>
+
 </head>
 <body>
     <div id="app">
+        @include('laratomics-workshop::navbar')
 
         <main class="py-4">
             @yield('content')

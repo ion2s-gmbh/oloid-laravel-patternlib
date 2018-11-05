157fd3764840cb93e3876ba7939f8fe7a5286e08
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-05 08:45:24 +0100

    - adding laratomics resources to view.php

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -3,6 +3,7 @@
 namespace Laratomics\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class InstallCommand extends Command
 {
@@ -33,8 +34,26 @@ class InstallCommand extends Command
         $this->comment('Publishing Laratomics Workshop Configuration...');
         $this->callSilent('vendor:publish', ['--tag' => 'workshop-config']);
 
+        $this->registerViewResources();
+
         $this->info('Laratomics Workshop installed successfully.');
 
         return 0;
+    }
+
+    private function registerViewResources()
+    {
+        $viewConfig = file_get_contents(config_path('view.php'));
+        $pathParts = explode('/', config('laratomics-workshop.basePath'));
+        $basePath = array_pop($pathParts);
+        $resourcePath = "resource_path('{$basePath}'),";
+        if (!Str::contains($viewConfig, $resourcePath)) {
+            file_put_contents(config_path('view.php'), str_replace(
+                "resource_path('views'),".PHP_EOL,
+                "resource_path('views'),".PHP_EOL."        ".$resourcePath.PHP_EOL,
+                $viewConfig
+            ));
+            $this->comment('Extra view resources have been added in view.php');
+        }
     }
 }

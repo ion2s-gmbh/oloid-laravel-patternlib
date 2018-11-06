3ac8363faf1b2559742a1476cc920599e9a0190f
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-06 14:49:54 +0100

    - introduced orchestra/testbench - prepared Unit testing

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Laratomics\Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}

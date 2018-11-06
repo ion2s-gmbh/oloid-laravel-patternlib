91994c4fd95a0a0e0fa581a9ec8d43e398d94f7e
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-06 15:09:36 +0100

    - finished preparation of automated tests

---
+++ tests/Unit/ExampleUnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Laratomics\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class ExampleUnitTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function basicUnitTest()
+    {
+        $this->assertTrue(true);
+    }
+}

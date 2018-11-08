93ab09c69578d39106f9a43177731602aa35b453
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-08 13:49:36 +0100

    - more testing on PatternController

--- tests/Integration/Http/Controllers/PatternControllerTest.php
+++ tests/Integration/Http/Controllers/PatternControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Laratomics\Tests\Integration;
 
+use Illuminate\Http\JsonResponse;
 use Laratomics\Tests\BaseTestCase;
 
 class PatternControllerTest extends BaseTestCase
@@ -28,22 +29,43 @@ class PatternControllerTest extends BaseTestCase
         $response = $this->post('workshop/api/v1/pattern', $data);
 
         // assert
-        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertEquals(JsonResponse::HTTP_CREATED, $response->getStatusCode());
         $response->assertJsonFragment($expectedJson);
     }
 
-//    /**
-//     * @test
-//     * @covers \Laratomics\Http\Controllers\PatternController
-//     */
-//    public function it_should_be_an_invalide_request()
-//    {
-//        // arrange
-//
-//        // act
-//
-//        // assert
-//        // TODO: implement
-//        $this->fail();
-//    }
+    /**
+     * @test
+     * @covers \Laratomics\Http\Controllers\PatternController
+     */
+    public function it_should_be_an_invalide_request_name_missing()
+    {
+        // arrange
+        $data = [
+            'description' => 'This is a test pattern'
+        ];
+
+        // act
+        $response = $this->postJson('workshop/api/v1/pattern', $data);
+
+        // assert
+        $this->assertEquals(JsonResponse::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+    }
+
+    /**
+     * @test
+     * @covers \Laratomics\Http\Controllers\PatternController
+     */
+    public function it_should_be_an_invalide_request_description_missing()
+    {
+        // arrange
+        $data = [
+            'name' => 'pages.testpage'
+        ];
+
+        // act
+        $response = $this->postJson('workshop/api/v1/pattern', $data);
+
+        // assert
+        $this->assertEquals(JsonResponse::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+    }
 }

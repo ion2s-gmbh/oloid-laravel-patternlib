f4b996c4cb989792f70f5e038e0c238b579b9e34
Sebastian <seb.baum@googlemail.com>
2018-11-20 20:05:04 +0100

    - first level navigation is working

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -2,11 +2,11 @@
 
   <section class="project">
 
-    
+
     <div class="project-info">
 
       <h1 class="project-name">{{ $store.state.appInfo.appName }}</h1>
-      
+
       <!-- <div class="project-updates">
         <button class="btn">
             <i class="fas fa-bell"></i>
@@ -16,13 +16,73 @@
     </div>
 
 
+    <!--<nav class="project-navigation">-->
+
+    <!--<ul class="patterns">-->
+    <!---->
+    <!--<li class="pattern u-center"-->
+    <!--:class="{ active: activeMainMenu === menu.name }"-->
+
+    <!--v-for="menu in navi.main"-->
+    <!--v-if="menu.items.length > 0">-->
+
+    <!--<span @click="toggleMainMenuItem(menu.name)">{{ menu.name }}</span>-->
+
+    <!--<ul class="patterns&#45;&#45;sub">-->
+
+    <!--<li class="pattern"-->
+    <!--v-for="item in menu.items"-->
+    <!--v-if="typeof item === 'object'">-->
+    <!-- TODO:  this must be active on click -  can't use the same logic here or else it will be automatically applied -->
+
+    <!--<div v-for="sub in item.subs"-->
+    <!--:class="{ active: activeSubMenu === sub.name}"-->
+    <!--@click="toggleSubMenuItem(sub.name)">-->
+    <!--{{ sub.name }}-->
+    <!--<i class="fas fa-caret-down"></i> &lt;!&ndash; TODO: Only display when has children &ndash;&gt;-->
+
+    <!--<ul class="patterns&#45;&#45;sub">-->
+
+    <!--<li class="pattern" v-for="subItem in sub.items">-->
+    <!--<router-link :to="{ name: 'preview', params: { pattern: `${menu.name}.${sub.name}.${subItem}` } }">-->
+    <!--{{ subItem }}-->
+    <!--</router-link>-->
+    <!--</li>-->
+
+    <!--</ul>-->
+
+    <!--</div>-->
+
+    <!--</li>-->
+
+    <!--<li class="pattern" v-else-if="typeof item !== 'object'">-->
+    <!--<router-link :to="{ name: 'preview', params: { pattern: `${menu.name}.${item}` } }">-->
+    <!--{{ item }}-->
+    <!--</router-link>-->
+    <!--</li>-->
+
+    <!--</ul>-->
+
+    <!--</li>-->
+
+    <!--</ul>-->
+
+    <!--&lt;!&ndash; <router-link :to="{ name: 'create' }">-->
+    <!---->
+    <!--<button class="btn btn&#45;&#45;create">-->
+    <!--<i class="fas fa-plus-square"></i>-->
+    <!--</button>-->
+
+    <!--</router-link> &ndash;&gt;-->
+
+    <!--</nav>-->
+
     <nav class="project-navigation">
 
       <ul class="patterns">
-        
+
         <li class="pattern u-center"
             :class="{ active: activeMainMenu === menu.name }"
-
             v-for="menu in navi.main"
             v-if="menu.items.length > 0">
 
@@ -30,36 +90,32 @@
 
           <ul class="patterns--sub">
 
+            <!--<li class="pattern" v-else-if="typeof item !== 'object'">-->
             <li class="pattern"
-                v-for="item in menu.items"
-                v-if="typeof item === 'object'">
-          <!-- TODO:  this must be active on click -  can't use the same logic here or else it will be automatically applied -->
-
-              <div v-for="sub in item.subs"
-                   :class="{ active: activeSubMenu === sub.name}"
-                   @click="toggleSubMenuItem(sub.name)">
-              {{ sub.name }}
-                <i class="fas fa-caret-down"></i> <!-- TODO: Only display when has children -->
-
-                <ul class="patterns--sub">
-
-                  <li class="pattern" v-for="subItem in sub.items">
-                    <router-link :to="{ name: 'preview', params: { pattern: `${menu.name}.${sub.name}.${subItem}` } }">
-                      {{ subItem }}
-                    </router-link>
-                  </li>
-
-                </ul>
-
-              </div>
-
-            </li>
-
-            <li class="pattern" v-else-if="typeof item !== 'object'">
+                v-if="typeof item !== 'object'"
+                v-for="item in menu.items">
               <router-link :to="{ name: 'preview', params: { pattern: `${menu.name}.${item}` } }">
                 {{ item }}
               </router-link>
             </li>
+
+            <!--<li class="pattern">-->
+            <!-- TODO:  this must be active on click -  can't use the same logic here or else it will be automatically applied -->
+
+            <!--<a href="">-->
+            <!--Buttons-->
+            <!--<i class="fas fa-caret-down"></i> &lt;!&ndash; TODO: Only display when has children &ndash;&gt;-->
+            <!--</a>-->
+
+            <!--<ul class="patterns&#45;&#45;sub">-->
+
+            <!--<li class="pattern">btn_submit</li>-->
+
+            <!--<li class="pattern">btn_form</li>-->
+
+            <!--</ul>-->
+
+            <!--</li>-->
 
           </ul>
 
@@ -67,16 +123,8 @@
 
       </ul>
 
-      <!-- <router-link :to="{ name: 'create' }">
-        
-        <button class="btn btn--create">
-          <i class="fas fa-plus-square"></i>
-        </button>
-
-      </router-link> -->
-
     </nav>
-    
+
   </section>
 
 </template>
@@ -90,6 +138,7 @@
     data() {
 
       return {
+        'active': false,
         'navi': {
           'main': [
             {
@@ -118,7 +167,7 @@
               ]
             },
             {
-              'name':'elements',
+              'name': 'elements',
               'items': []
             }
           ]
@@ -128,27 +177,21 @@
       }
     },
     methods: {
-      toggleMainMenuItem: function(menu) {
+      toggleMainMenuItem: function (menu) {
         if (this.activeMainMenu === menu) {
           this.activeMainMenu = '';
         } else {
           this.activeMainMenu = menu;
         }
       },
-      toggleSubMenuItem: function(subMenu) {
+      toggleSubMenuItem: function (subMenu) {
         if (this.activeMainMenu === subMenu) {
           this.activeSubMenu = '';
         } else {
           this.activeSubMenu = subMenu;
         }
       }
-    },
-
-    // computed: {
-    //   isActive: function () {
-        // return this.activeMainMenu === menu;
-      // }
-    // }
+    }
 
   }
 

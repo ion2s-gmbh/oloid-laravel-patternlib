40b7c084008c008c85679c595b99db2004200f87
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-22 14:53:33 +0100

    - documentation

--- src/Http/Controllers/NavigationController.php
+++ src/Http/Controllers/NavigationController.php
@@ -7,12 +7,33 @@ use Illuminate\Http\JsonResponse;
 
 class NavigationController extends Controller
 {
-    private function getNavi($path)
+    /**
+     * Get the navigation
+     *
+     * @return JsonResponse
+     */
+    public function get(): JsonResponse
+    {
+        return JsonResponse::create([
+            'data' => $this->getNavi(config('workshop.patternPath'))
+        ]);
+    }
+
+    /**
+     * Recursive method to build the navigation json structure.
+     *
+     * @param $path
+     * @return array
+     */
+    private function getNavi($path): array
     {
         $data = [];
         $fs = new Filesystem();
         $directories = $fs->directories($path);
 
+        /*
+         * Check every nested directory for files.
+         */
         foreach ($directories as $directory) {
             $data[] = [
                 'name' => str_after($directory, $path . '/'),
@@ -21,8 +42,10 @@ class NavigationController extends Controller
             ];
         }
 
+        /*
+         * Process files, that are contained in the current path.
+         */
         $files = $fs->files($path);
-
         foreach ($files as $pattern) {
             if (ends_with($pattern, '.blade.php')) {
                 $name = str_after(str_before($pattern, '.blade.php'), $path . '/');
@@ -36,67 +59,5 @@ class NavigationController extends Controller
         }
 
         return $data;
-    }
-
-    /**
-     * Get the navigation
-     * @return JsonResponse
-     */
-    public function get(): JsonResponse
-    {
-        return JsonResponse::create([
-            'data' => $this->getNavi(config('workshop.patternPath'))
-        ]);
-
-        return JsonResponse::create(
-            [
-                'data' => [
-                    [
-                        'name' => 'atoms',
-                        'path' => 'atoms',
-                        'items' => [
-//                            [
-//                                'name' => 'buttons',
-//                                'path' => 'atoms.buttons',
-//                                'items' => [
-//                                    [
-//                                        'name' => 'positive',
-//                                        'path' => 'atoms.buttons.positive',
-//                                        'items' => [
-//                                            [
-//                                                'name' => 'submit',
-//                                                'path' => 'atoms.buttons.positive.submit',
-//                                                'items' => []
-//                                            ]
-//                                        ]
-//                                    ]
-//                                ]
-//                            ],
-                            [
-                                'name' => 'headlines',
-                                'path' => 'atoms.headlines',
-                                'items' => [
-                                    [
-                                        'name' => 'headline1',
-                                        'path' => 'atoms.headlines.headline1',
-                                        'items' => []
-                                    ]
-                                ]
-                            ],
-                        ]
-                    ],
-//                    [
-//                        'name' => 'landingpage',
-//                        'path' => 'landingpage',
-//                        'items' => []
-//                    ],
-//                    [
-//                        'name' => 'about',
-//                        'path' => 'about',
-//                        'items' => []
-//                    ]
-                ]
-            ]
-        );
     }
 }

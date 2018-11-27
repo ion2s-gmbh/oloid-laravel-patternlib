3b75e44dcd43a3cf8106da48c82b8688347f9308
Sebastian Baum <sebastian.baum@ion2s.com>
2018-11-27 10:06:54 +0100

    - added helper methods and tests

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,19 @@
 <?php
 
 
+use Illuminate\Filesystem\Filesystem;
 use Laratomics\Exceptions\RenderingException;
 
-if (! function_exists('compileBladeString')) {
+if (!function_exists('compile_blade_string')) {
     /**
+     * Compiles a string containing Blade content to shippable HTML.
+     *
      * @param $value
      * @param array $args
      * @return false|string
      * @throws RenderingException
      */
-    function compileBladeString($value, array $args = array())
+    function compile_blade_string($value, array $args = array()): string
     {
         $generated = Blade::compileString($value);
 
@@ -19,16 +22,14 @@ if (! function_exists('compileBladeString')) {
         // We'll include the view contents for parsing within a catcher
         // so we can avoid any WSOD errors. If an exception occurs we
         // will throw it out to the exception handler.
-        try
-        {
-            eval('?>'.$generated);
+        try {
+            eval('?>' . $generated);
         }
 
             // If we caught an exception, we'll silently flush the output
             // buffer so that no partially rendered views get thrown out
             // to the client and confuse the user with junk.
-        catch (Exception $e)
-        {
+        catch (Exception $e) {
             ob_get_clean();
             throw new RenderingException('Preview rendering failed', 0, $e);
         }
@@ -36,5 +37,133 @@ if (! function_exists('compileBladeString')) {
         $content = ob_get_clean();
 
         return $content;
+    }
+}
+
+if (!function_exists('pattern_path')) {
+    /**
+     * Returns the absolute path to the patterns directory.
+     *
+     * @param string $subpath
+     * @return \Illuminate\Config\Repository|mixed
+     */
+    function pattern_path(string $subpath = ''): string
+    {
+        $path = config('workshop.patternPath');
+        if (!empty($subpath)) {
+            $path = config('workshop.patternPath') . "/{$subpath}";
+        }
+
+        return $path;
+    }
+}
+
+if (!function_exists('dotted_path')) {
+    /**
+     * Converts a slash separarted path to the dotted notation.
+     * Surrounding slashes and whitespaces will be stripped.
+     *
+     * E.g. /some/path/to/something/ => some.path.to.something
+     *
+     * @param $path
+     * @return string
+     */
+    function dotted_path($path): string
+    {
+        $path = trim($path, "/ \t\n\r\0\x0B");
+        return str_replace('/', '.', $path);
+    }
+}
+
+if (!function_exists('slash_path')) {
+    /**
+     * Converts a dot separated path to the slash notation.
+     * E.g. some.path.to.something => some/path/to/something
+     *
+     * @param $path
+     * @return string
+     */
+    function slash_path($path): string
+    {
+        return str_replace('.', '/', $path);
+    }
+}
+
+if (!function_exists('parent_dir')) {
+    /**
+     * Given a path to a file/directory, this method returns the path, without the last part.
+     * That is the directory path where the file/directory is contained.
+     * E.g. /some/path/to/a/file.txt => /some/path/to/a
+     *
+     * @param $path
+     * @return string
+     */
+    function parent_dir($path): string
+    {
+        $parts = explode('/', $path);
+        array_pop($parts);
+        return implode('/', $parts);
+    }
+}
+
+if (!function_exists('dir_is_empty')) {
+    function dir_is_empty($path)
+    {
+        $fs = new Filesystem();
+        $files = $fs->files($path);
+        $directories = $fs->directories($path);
+
+        return count($files) === 0
+            && count($directories) === 0;
+    }
+}
+
+if (!function_exists('dir_contains_any')) {
+    /**
+     * Check if the given directory contains files with a specific extension.
+     *
+     * @param $directory
+     * @param $extension
+     * @return bool
+     */
+    function dir_contains_any($directory, $extension): bool
+    {
+        $fs = new Filesystem();
+        $files = $fs->files($directory);
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                if (ends_with($file, $extension)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+if (!function_exists('root_dir')) {
+    /**
+     * Get the root of a Pattern.
+     * E.g. atoms.buttons.submit => atoms
+     *
+     * @param $pattern
+     * @return mixed
+     */
+    function pattern_root($pattern): string
+    {
+        $parts = explode('.', $pattern);
+        $root = array_shift($parts);
+        return $root;
+    }
+}
+
+if (!function_exists('remove_empty_branch')) {
+    function remove_empty_branch($branch, $rootDir)
+    {
+//        $fs = new Filesystem();
+//        if ($rootDir !== parent_dir($branch)) {
+//            $fs->deleteDirectory($branch);
+//            remove_empty_branch($rootDir, parent_dir($branch));
+//        }
     }
 }
---
+++ tests/Unit/HelperMethodsTest.php
@@ -0,0 +1,207 @@
+<?php
+
+
+namespace Unit;
+
+
+use Exception;
+use Illuminate\Filesystem\Filesystem;
+use Laratomics\Tests\BaseTestCase;
+use Laratomics\Tests\Traits\TestStubs;
+
+class HelperMethodsTest extends BaseTestCase
+{
+    use TestStubs;
+
+    /**
+     * @test
+     * @covers ::compile_blade_string
+     */
+    public function it_should_parse_a_php_template_to_html()
+    {
+        // arrange
+        $template = '<h1>{{ $text }}</h1>';
+
+        // act
+        try {
+            $html = compile_blade_string($template, ['text' => 'TEST']);
+            $this->assertEquals('<h1>TEST</h1>', $html);
+        } catch (Exception $e) {
+            $this->fail();
+        }
+    }
+
+    /**
+     * @test
+     * @covers ::pattern_path
+     */
+    public function it_should_return_the_pattern_base_path()
+    {
+        $this->preparePatternStub();
+
+        // assert
+        $expectedPatternPath = realpath(__DIR__ . '/../tmp/patterns');
+        $this->assertEquals($expectedPatternPath, pattern_path());
+    }
+
+    /**
+     * @test
+     * @covers ::pattern_path
+     */
+    public function it_should_return_a_subpath_within_the_patttern_path()
+    {
+        // arrange
+        $this->preparePatternStub();
+
+        // assert
+        $expectedPatternPath = realpath(__DIR__ . '/../tmp/patterns/atoms');
+        $this->assertEquals($expectedPatternPath, pattern_path('atoms'));
+    }
+
+    /**
+     * @test
+     * @covers ::dir_is_empty
+     */
+    public function it_should_check_if_a_given_directory_is_empty()
+    {
+        // arrange
+        $testDirectory = "{$this->tempDir}/empty/subdirectory";
+        $fs = new Filesystem();
+        $fs->makeDirectory($testDirectory, 0755, true);
+
+        // assert
+        $this->assertTrue(dir_is_empty($testDirectory));
+    }
+
+    /**
+     * @test
+     * @covers ::dir_is_empty
+     */
+    public function it_should_check_if_a_given_directory_is_not_empty()
+    {
+        // arrange
+        $testDirectory = "{$this->tempDir}/empty/subdirectory";
+        $fs = new Filesystem();
+        $fs->makeDirectory($testDirectory, 0755, true);
+
+        // assert
+        $this->assertFalse(dir_is_empty("{$this->tempDir}/empty"));
+    }
+
+    /**
+     * @test
+     * @covers ::dir_is_empty
+     */
+    public function it_should_check_if_a_given_directory_contains_files()
+    {
+        // arrange
+        $this->preparePatternStub();
+
+        // assert
+        $this->assertFalse(dir_is_empty("{$this->tempDir}/patterns"));
+    }
+
+    /**
+     * @test
+     * @covers ::dir_contains_any
+     */
+    public function it_should_check_that_a_directory_contains_any_files_of_a_given_extension()
+    {
+        // arrange
+        $this->preparePatternStub();
+        $directory = "{$this->tempDir}/patterns/atoms/text";
+
+        // assert
+        $this->assertTrue(dir_contains_any($directory, 'blade.php'));
+    }
+
+    /**
+     * @test
+     * @covers ::dir_contains_any
+     */
+    public function it_should_check_if_a_directory_not_contains_any_files_of_a_given_extension()
+    {
+        // arrange
+        $this->preparePatternStub();
+        $directory = "{$this->tempDir}/patterns/atoms/text";
+
+        // assert
+        $this->assertFalse(dir_contains_any($directory, 'txt'));
+    }
+
+    /**
+     * @test
+     * @covers ::dotted_path
+     */
+    public function it_should_convert_a_slash_separated_path_to_dotted_notation()
+    {
+        // arrange
+        $path = '/var/some/test/path/';
+        $expected = 'var.some.test.path';
+        $this->assertEquals($expected, dotted_path($path));
+    }
+
+    /**
+     * @test
+     * @covers ::slash_path
+     */
+    public function it_should_convert_a_dot_separated_path_to_slash_notation()
+    {
+        // arrange
+        $dottedPath = 'var.some.test.path';
+        $expected = 'var/some/test/path';
+        $this->assertEquals($expected, slash_path($dottedPath));
+    }
+
+    /**
+     * @test
+     * @covers ::parent_dir
+     */
+    public function it_should_return_the_directory_where_the_given_file_is_contained()
+    {
+        // arrange
+        $file = '/some/path/to/a/file.txt';
+        $expected = '/some/path/to/a';
+
+        // assert
+        $this->assertEquals($expected, parent_dir($file));
+    }
+
+    /**
+     * @test
+     * @covers ::root_dir
+     */
+    public function it_should_return_the_patterns_root_dir()
+    {
+        // arrange
+        $pattern = 'root.path.to.a.file.txt';
+
+        // assert
+        $this->assertEquals('root', pattern_root($pattern));
+    }
+
+    /**
+     * @test
+     * @covers ::remove_empty_branch
+     */
+    public function it_should_remove_a_path_branch_that_does_not_contain_any_template_files()
+    {
+        $this->markTestIncomplete();
+        // arrange
+        $testDirectory = "{$this->tempDir}/root/ab/cd";
+        $fs = new Filesystem();
+        $fs->makeDirectory($testDirectory, 0755, true);
+
+        $patternRoot = "{$this->tempDir}/root";
+        $branch = $testDirectory;
+
+        // act
+        remove_empty_branch($patternRoot, $branch);
+
+        // assert
+        $this->assertFalse($fs->exists("{$this->tempDir}/root/ab/cd"));
+        $this->assertFalse($fs->exists("{$this->tempDir}/root/ab"));
+        $this->assertFalse($fs->exists("{$this->tempDir}/root"));
+        $this->assertTrue($fs->exists($this->tempDir));
+    }
+}

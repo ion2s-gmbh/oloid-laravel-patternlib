688a72f266d02ee1ecc6d6a08705673cc91f9797
Sebastian Baum <sebastian.baum@ion2s.com>
2019-01-09 13:40:48 +0100

    - fixed foreach and include rendering issue

--- src/helpers.php
+++ src/helpers.php
@@ -15,28 +15,8 @@ if (!function_exists('compile_blade_string')) {
      */
     function compile_blade_string($value, array $args = []): string
     {
-        $generated = Blade::compileString($value);
-
-        ob_start() and extract($args, EXTR_SKIP);
-
-        // We'll include the view contents for parsing within a catcher
-        // so we can avoid any WSOD errors. If an exception occurs we
-        // will throw it out to the exception handler.
-        try {
-            eval('?>' . $generated);
-        }
-
-            // If we caught an exception, we'll silently flush the output
-            // buffer so that no partially rendered views get thrown out
-            // to the client and confuse the user with junk.
-        catch (Exception $e) {
-            ob_get_clean();
-            throw new RenderingException('Preview rendering failed', 0, $e);
-        }
-
-        $content = ob_get_clean();
-
-        return $content;
+        $path = "patterns.{$value}";
+        return view($path, $args);
     }
 }
 

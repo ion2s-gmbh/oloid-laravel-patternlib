5f8549c50f954ea2e24d49be12ec2df10f51ad61
Sebastian Baum <sebastian.baum@ion2s.com>
2019-03-06 09:52:19 +0100

    - testing the GitService

--- src/Services/GitService.php
+++ src/Services/GitService.php
@@ -4,13 +4,49 @@
 namespace Oloid\Services;
 
 
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+
 class GitService
 {
     /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * GitService constructor.
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+
+    /**
+     * Get the current git branch of the project
      * @return string
      */
     public function getCurrentBranch(): string
     {
-        return 'service';
+        /*
+         * If there is no .git folder, return. There is no current branch.
+         */
+        if (!$this->filesystem->exists(base_path('../.git'))) {
+            return 'No git repo';
+        }
+
+        /*
+         * Get the current git branch
+         */
+        $process = new Process('git rev-parse --abbrev-ref HEAD',
+            base_path(),
+            null,
+            null,
+            600);
+        $process->run();
+
+        return trim($process->getOutput());
     }
 }
--- tests/Unit/Services/GitServiceTest.php
+++ tests/Unit/Services/GitServiceTest.php
@@ -2,9 +2,62 @@
 
 namespace Unit\Services;
 
+use Illuminate\Filesystem\Filesystem;
+use Mockery;
+use Oloid\Services\GitService;
 use Tests\BaseTestCase;
 
 class GitServiceTest extends BaseTestCase
 {
+    /**
+     * @test
+     * @covers \Oloid\Services\GitService
+     */
+    public function it_should_return_no_branch_without_git()
+    {
+        // arrange
+        $fileSystemMock = Mockery::mock(Filesystem::class);
+        $fileSystemMock->shouldReceive('exists')
+            ->with(base_path('../.git'))
+            ->andReturnFalse();
 
+        $this->app->bind(Filesystem::class, function ($app) use ($fileSystemMock) {
+            return $fileSystemMock;
+        });
+
+        // act
+        /** @var GitService $cut */
+        $cut = app(GitService::class);
+
+
+        // assert
+        $this->assertEquals('No git repo', $cut->getCurrentBranch());
+    }
+
+    /**
+     * @test
+     * @covers \Oloid\Services\GitService
+     */
+    public function it_should_return_current_git_branch()
+    {
+        // arrange
+        $fileSystemMock = Mockery::mock(Filesystem::class);
+        $fileSystemMock->shouldReceive('exists')
+            ->with(base_path('../.git'))
+            ->andReturnTrue();
+
+        $this->app->bind(Filesystem::class, function ($app) use ($fileSystemMock) {
+            return $fileSystemMock;
+        });
+
+        // act
+        /** @var GitService $cut */
+        $cut = app(GitService::class);
+
+        // assert
+        $currentBranch = $cut->getCurrentBranch();
+        $this->assertNotNull($currentBranch);
+        $this->assertNotEmpty($currentBranch);
+        $this->assertNotEquals('No git repo', $currentBranch);
+    }
 }
